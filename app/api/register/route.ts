24258ef39f5755474a2a1163b9fba5f4404b2a14
Junnygram <you@example.com>
2023-10-07 01:11:01 +0100

    fixing deployment issue

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -5,8 +5,12 @@ import bcrypt from 'bcrypt';
 export async function POST(request: Request) {
   const body = await request.json();
 
-  const { firstName, lastName, email, password } = body;
+  // const { firstName, lastName, email, password } = body;
 
+  const firstName: string = body.firstName;
+  const lastName: string = body.lastName;
+  const email: string = body.email;
+  const password: string = body.password;
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
