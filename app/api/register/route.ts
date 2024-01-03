52566ee24c44acce7f6c4b9392d3d7632c7ca4e8
Junnygram <you@example.com>
2024-01-03 15:04:03 +0100

    chore : working ...

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../lib/prismadb';
 import bcrypt from 'bcrypt';
+import { getUserByEmail } from '@/lib/data/user';
+import { sendVerificationEmail } from '@/lib/mail';
+import { generateVerificationToken } from '@/lib/data/token';
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -8,6 +11,11 @@ export async function POST(request: Request) {
   const { firstName, lastName, email, password } = body;
   const hashedPassword = await bcrypt.hash(password, 12);
 
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
   const user = await prisma.user.create({
     data: {
       firstName,
@@ -17,5 +25,8 @@ export async function POST(request: Request) {
     },
   });
 
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
   return NextResponse.json(user);
+  // return { NextResponse.json(user), success: "Confirmation email sent!" }};
 }
